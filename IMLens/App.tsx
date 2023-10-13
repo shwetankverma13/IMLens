9f3b6062ee9eb2e14fc773a03b23fe6715c1c8d4
Shwetank Verma <ShwetankVerma@IM-HYD-LAP-0247.local>
2023-10-13 19:28:41 +0530

    Navigation + Details Page

--- IMLens/App.tsx
+++ IMLens/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import {PaperProvider} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import ProductDescription from '../IMLens/src/Pages/DetailsPage/index';
 import Landing from './src/Pages/Landing';
 
-function App(): JSX.Element {
+const stack = createNativeStackNavigator();
+const Navigation = () => {
   return (
-    <PaperProvider>
-      <Landing />
-    </PaperProvider>
+    <NavigationContainer>
+      <stack.Navigator
+        screenOptions={{headerShown: false}}
+        initialRouteName="landing">
+        <stack.Screen name="landing" component={Landing} />
+        <stack.Screen
+          name="productDescription"
+          component={ProductDescription}
+        />
+      </stack.Navigator>
+    </NavigationContainer>
   );
-}
-export default App;
+};
+
+export default Navigation;
--- IMLens/src/Pages/Landing/index.tsx
+++ IMLens/src/Pages/Landing/index.tsx
@@ -3,14 +3,16 @@ import {View, Text, TouchableOpacity} from 'react-native';
 import styles from './styles';
 import Headers from '../../Components/Header';
 
-function Landing(): JSX.Element {
+function Landing({navigation}: any): JSX.Element {
   return (
     <>
       <Headers />
       <View style={styles.container}>
         <Text style={styles.title}>Welcome to IMLens</Text>
         <Text style={styles.subtitle}>Your Catchy Subtitle Here</Text>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('productDescription')}>
           <Text style={styles.buttonText}>Get Started</Text>
         </TouchableOpacity>
       </View>
