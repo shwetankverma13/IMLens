4e2fec6105f777ae94c49f39bcd8ff1e048a1dde
Amar Shukla <AmarShukla@IM-HYD-LAP-0243.local>
2023-10-13 12:01:52 +0530

    skeleton project

--- IMLens/ios/IMLensTests/IMLensTests.m
+++ IMLens/ios/IMLensTests/IMLensTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface IMLensTests : XCTestCase
+@interface imlensTests : XCTestCase
 
 @end
 
-@implementation IMLensTests
+@implementation imlensTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
