07b61f6b83ec14881dd667d89403771b8750824b
Sahil Singh <SahilSingh@IM-HYD-LAP-0230.local>
2023-10-14 09:51:43 +0530

    minor change

--- IMLens/src/Pages/Catalogue/index.tsx
+++ IMLens/src/Pages/Catalogue/index.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {View, ScrollView, TouchableOpacity} from 'react-native';
+import {View, ScrollView, TouchableOpacity, Text} from 'react-native';
 import {Skeleton} from '@rneui/base';
 import {Image} from 'react-native-elements';
 import {
@@ -73,6 +73,9 @@ const CatalogPage = ({navigation}: any) => {
             </Button>
           </View>
         </View>
+      </View>
+      <View>
+        <Text>No matching products found. Here are some other products</Text>
       </View>
       <Title
         style={{
